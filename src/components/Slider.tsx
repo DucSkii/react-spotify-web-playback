246070b0b84a3255e2b434467592152c649b0ed1
DucSkii <ducvietdao@live.co.uk>
2021-03-25 20:41:16 +0000

    changed slider wrapper

--- src/components/Slider.tsx
+++ src/components/Slider.tsx
@@ -17,6 +17,8 @@ const Wrapper = styled('div')(
     position: 'relative',
     transition: 'height 0.3s',
     zIndex: 10,
+    width: '100%',
+    justifyContent: 'center',
   },
   ({ style }: StyledProps) => ({
     height: px(style.sliderHeight),
@@ -55,7 +57,6 @@ export default class Slider extends React.PureComponent<Props> {
               rangeColor: styles.sliderColor,
               trackBorderRadius: styles.sliderTrackBorderRadius,
               trackColor: styles.sliderTrackColor,
-              width: '33.3333%',
             },
           }}
           x={position}
