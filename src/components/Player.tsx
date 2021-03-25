b4fe8707f199c64628d302da73c1c46bc8a8188e
DucSkii <ducvietdao@live.co.uk>
2021-03-25 11:34:51 +0000

    changed to blue

--- src/components/Player.tsx
+++ src/components/Player.tsx
@@ -40,7 +40,7 @@ const Player = React.forwardRef<HTMLDivElement, ComponentsProps>(
       <div
         ref={ref}
         className="PlayerRSWP"
-        style={{ backgroundColor: 'red', minHeight: px(height) }}
+        style={{ backgroundColor: 'blue', minHeight: px(height) }}
       >
         {children}
       </div>
